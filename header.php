68f664f41834c971978163a04086affbe94336bf
J White <jody@jody-white.com>
2013-12-20 16:09:12 -0500

    Fixed <title> in header. added <?php bloginfo('name'); ?>

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php wp_title(); ?> </title>
+    <title><?php bloginfo('name'); ?><?php wp_title(); ?> </title>
 
     <meta name="description" content="Documentation and reference library for ZURB Foundation. JavaScript, CSS, components, grid and more." />
     
